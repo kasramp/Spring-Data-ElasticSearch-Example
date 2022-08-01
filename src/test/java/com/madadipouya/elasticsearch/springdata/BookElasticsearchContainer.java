f4771882081faa7782221fa637a99a52bfe0da14
Jarjanazy <celil.cercenazi@gmail.com>
2022-08-01 23:15:17 +0200

    fix elasticsearch container

--- src/test/java/com/madadipouya/elasticsearch/springdata/BookElasticsearchContainer.java
+++ src/test/java/com/madadipouya/elasticsearch/springdata/BookElasticsearchContainer.java
@@ -1,6 +1,7 @@
 package com.madadipouya.elasticsearch.springdata;
 
 import org.testcontainers.elasticsearch.ElasticsearchContainer;
+import org.testcontainers.utility.DockerImageName;
 
 public class BookElasticsearchContainer extends ElasticsearchContainer {
 
@@ -11,7 +12,8 @@ public class BookElasticsearchContainer extends ElasticsearchContainer {
     private static final String ELASTIC_SEARCH = "elasticsearch";
 
     public BookElasticsearchContainer() {
-        super(ELASTIC_SEARCH_DOCKER);
+        super(DockerImageName.parse(ELASTIC_SEARCH_DOCKER)
+                .asCompatibleSubstituteFor("docker.elastic.co/elasticsearch/elasticsearch"));
         this.addFixedExposedPort(9200, 9200);
         this.addFixedExposedPort(9300, 9300);
         this.addEnv(CLUSTER_NAME, ELASTIC_SEARCH);
